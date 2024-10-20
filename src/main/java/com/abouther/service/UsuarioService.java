0badd8a4fb3449752b6b3faa51e77e48383d5e10
carlalopesj <carlaljovito@gmail.com>
2024-10-19 23:14:09 -0300

    add: services

---
+++ src/main/java/com/abouther/service/UsuarioService.java
@@ -0,0 +1,53 @@
+package com.abouther.service;
+
+import java.util.List;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.abouther.DAO.IUsuario;
+import com.abouther.model.Usuario;
+
+@Service
+public class UsuarioService {
+    
+    private IUsuario dao;
+    private PasswordEncoder passwordEncoder;
+
+    public UsuarioService(IUsuario dao) {
+        this.dao = dao;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    public List<Usuario> listarUsuario() {
+        List<Usuario> lista = dao.findAll();
+        return lista;
+    }
+
+    public Usuario criarUsuario(Usuario usuario) {
+        String encoder = this.passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(encoder);
+        Usuario usuarioNovo = dao.save(usuario);
+        return usuarioNovo;
+    }
+
+    //@SuppressWarnings("deprecation")
+    public Boolean validarSenha(Usuario usuarioEncontrado) {
+        // Adicione logs para verificar as senhas
+        System.out.println("Senha para validar: " + usuarioEncontrado.getSenha());
+    
+        Usuario usuarioBD = dao.findByEmail(usuarioEncontrado.getEmail());
+        if (usuarioBD == null) {
+            System.out.println("Usuário não encontrado.");
+            return false;
+        }
+    
+        System.out.println("Senha armazenada: " + usuarioBD.getSenha());
+    
+        Boolean isValid = passwordEncoder.matches(usuarioEncontrado.getSenha(), usuarioBD.getSenha());
+        System.out.println("Senha válida: " + isValid);
+        return isValid;
+    }
+
+}
